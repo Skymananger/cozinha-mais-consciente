a3f951c6c3ae9381e23475d3a5ba5702277d0ed4
Skymananger <skydistribuidoraroyal@gmail.com>
2026-03-13 18:38:05 -0300

    Add Google Site Verification meta tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,6 +25,9 @@ export const metadata: Metadata = {
   },
   twitter: { card: "summary_large_image" },
   robots: { index: true, follow: true },
+  verification: {
+    google: "Gta_5tOi01rPBFGLDdauW6J8678NuwIo7vFYjWzHJMA",
+  },
 };
 
 import CookieBanner from "@/components/CookieBanner";
